d3f0761cb0661c54ef7ed6cec175f21cda1f7936
m&m <113959750+MandMcoding@users.noreply.github.com>
2023-09-01 19:29:15 +0400

    Update Medical_Password_Generator (Run this file for password).py

--- Medical_Password_Generator (Run this file for password).py
+++ Medical_Password_Generator (Run this file for password).py
@@ -10,19 +10,27 @@ import random as r
 
 #Input
 print('Welcome to Medical Password Generator')
+type = input('Would you like to include medical words, or pharmaceutical drugs, or both? (medical/drugs/both) ').lowercase()
 length = int(input('How many words is the password? '))
 
 #Turning the txt file into a list (in order to pick a random word)
-password = ''
+file = 'medical_words_list.txt' if type == 'medical' else 'prescription_pharmaceutical_drugs_medications_list.txt' if type == 'drugs' else None
 med_words_list = []
-with open('medical_words_list.txt', 'r') as med_words:
-    for word in med_words:
-        med_words_list.append(word)
+if file != None:
+    with open(file, 'r') as med_words:
+        for word in med_words:
+            med_words_list.append(word)
+elif type == 'both':
+    with open('medical_words_list.txt') as f1, open('prescription_pharmaceutical_drugs_medications_list.txt') as f2:
+        for file in (f1, f2):
+            for word in file:
+                med_words_list.append(word.strip())
 
+password = ''
 #Making the password by repeadily picking random medical words
 for i in range(length):
     rand_index = r.randint(1,2053)
-    password += med_words_list[rand_index].replace("\n","").capitalize()
-    
+    password += med_words_list[rand_index].capitalize()
+
 #output
 print(password)
